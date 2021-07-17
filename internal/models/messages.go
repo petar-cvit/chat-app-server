bb1161a1028868ac9553240933e7962f88ad6600
petar-cvit <petar.cvitanovic@gmail.com>
2021-07-17 19:28:49 +0200

    messages model

--- internal/models/messages.go
+++ internal/models/messages.go
@@ -1,5 +1,15 @@
 package models
 
 type Messages struct {
-	Messages []string `json:"messages"`
+	Messages []*Message `json:"messages"`
+}
+
+type Message struct {
+	Text   string `json:"text"`
+	Time   string `json:"time"`
+	Issuer string `json:"issuer"`
+}
+
+func (m *Messages) AppendMessage(message *Message) {
+	m.Messages = append(m.Messages, message)
 }
